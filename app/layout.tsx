e8fc3dbeba62b5bbdb3a110b3fb373b97cbd8e93
Appon Islam <appon2003@gmail.com>
2026-04-11 04:27:43 +0600

    project updated with new packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
+          attribute="class"
           defaultTheme="dark"
           enableSystem
+          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
